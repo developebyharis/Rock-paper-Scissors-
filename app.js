ebf75ec2360685381b67eaa1a6f870fdf2277bb9
Muhammad Haris Khan <developewithharis@gmail.com>
2022-07-23 17:29:34 +0500

    Second Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    //Start the Game
+    const startGame = () => {
+        const playBtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match");
+
+ playBtn.addEventListener("click", () => {
+    introScreen.classList.add("fadeOut");
+    match.classList.add("fadeIn");
+ });
+};
+// play match 
+const playMatch = () => {
+    const options = document.querySelectorAll(".option button");
+    const playerHand = document.querySelectorAll("player-hand");
+    const computerHand = document.querySelectorAll(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
+    hands.forEach = (hand => {
+        hand.addEventListener("animationend", function() {
+            this.style.animation = "";
+    
+      });
+    
+    });
+    // computer option 
+    const computerOption = ["rock", "paper", "scissors"];
+    options.forEach(option => {
+        option.addEventListener("click", function() {
+            
+        })
+    })
+
+    }
+}
+
+
+
